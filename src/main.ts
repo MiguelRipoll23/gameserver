e5e0f0c2908364af82df3a51390356584b1775d5
Miguel Ripoll <3296866+MiguelRipoll23@users.noreply.github.com>
2025-04-30 12:49:29 +0200

    Add server notification cron job to main.ts (#15)

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,17 @@
 import { Container } from "@needle-di/core";
 import { HTTPService } from "./core/services/http-service.ts";
 import { KVService } from "./core/services/kv-service.ts";
+import { NOTIFICATION_EVENT } from "./api/versions/v1/constants/event-constants.ts";
+
+Deno.cron("Test server notification", "*/10 * * * *", () => {
+  dispatchEvent(
+    new CustomEvent(NOTIFICATION_EVENT, {
+      detail: {
+        message: `This is a server notification at ${new Date().toISOString()}`,
+      },
+    })
+  );
+});
 
 const container = new Container();
 
