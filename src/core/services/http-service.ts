98562a1e06eb0938a9c5dff8e65224cb73e1efba
Miguel Ripoll <3296866+MiguelRipoll23@users.noreply.github.com>
2025-05-11 20:07:48 +0200

    Implement body size limit middleware in HTTPService

--- src/core/services/http-service.ts
+++ src/core/services/http-service.ts
@@ -1,4 +1,5 @@
 import { logger } from "hono/logger";
+import { bodyLimit } from "hono/body-limit";
 import { serveStatic } from "hono/deno";
 import { OpenAPIHono } from "@hono/zod-openapi";
 import { inject, injectable } from "@needle-di/core";
@@ -9,6 +10,7 @@ import { ErrorHandlingService } from "./error-handling-service.ts";
 import { HonoVariablesType } from "../types/hono-variables-type.ts";
 import { CORSMiddleware } from "../middlewares/cors-middleware.ts";
 import { CacheMiddleware } from "../middlewares/cache-middleware.ts";
+import { ServerError } from "../../api/versions/v1/models/server-error.ts";
 
 @injectable()
 export class HTTPService {
@@ -16,7 +18,7 @@ export class HTTPService {
 
   constructor(
     private rootRooter = inject(RootRouter),
-    private apiRouter = inject(APIRouter),
+    private apiRouter = inject(APIRouter)
   ) {
     this.app = new OpenAPIHono();
     this.configure();
@@ -40,6 +42,23 @@ export class HTTPService {
     this.app.use("*", CORSMiddleware.create());
     this.app.use("*", CacheMiddleware.create());
     this.app.use("*", serveStatic({ root: "./static" }));
+    this.setBodyLimitMiddleware();
+  }
+
+  private setBodyLimitMiddleware(): void {
+    this.app.use(
+      "*",
+      bodyLimit({
+        maxSize: 1024 * 1024,
+        onError: () => {
+          throw new ServerError(
+            "BODY_SIZE_LIMIT_EXCEEDED",
+            "Request body size limit exceeded",
+            413
+          );
+        },
+      })
+    );
   }
 
   private setRoutes(): void {
