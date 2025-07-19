5ffad199834dfa06d8a515186fa10bcb3298d60e
Miguel Ripoll <3296866+MiguelRipoll23@users.noreply.github.com>
2025-07-19 16:06:46 +0200

    Update ban endpoint units and validation

--- src/api/versions/v1/schemas/moderation-schemas.ts
+++ src/api/versions/v1/schemas/moderation-schemas.ts
@@ -6,15 +6,20 @@ export const BanUserRequestSchema = z.object({
     .length(32)
     .describe("The user ID to ban")
     .openapi({ example: "123e4567e89b12d3a456426614174000" }),
-  reason: z.string().min(1).describe("Reason for the ban").openapi({
-    example: "Toxic behaviour",
-  }),
+  reason: z
+    .string()
+    .min(1)
+    .max(100)
+    .describe("Reason for the ban")
+    .openapi({
+      example: "Toxic behaviour",
+    }),
   duration: z
     .string()
-    .regex(/^[1-9]\d*(min|h|d|w|m|y)$/)
+    .regex(/^[1-9]\d*(?:mo|m|h|d|w|y)$/)
     .optional()
     .describe(
-      "Relative ban duration. Use 'min' for minutes, 'h' for hours, 'd' for days, 'w' for weeks, 'm' for months and 'y' for years",
+      `Relative ban duration. Supported units:\n- 'm' for minutes\n- 'h' for hours\n- 'd' for days\n- 'w' for weeks\n- 'mo' for months\n- 'y' for years`,
     )
     .openapi({ example: "1h" }),
 });
