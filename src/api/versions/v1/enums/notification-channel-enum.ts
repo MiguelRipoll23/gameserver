54ce51344a19fbd58e3c1aa1d01b27b0d04e2707
Miguel Ripoll <3296866+MiguelRipoll23@users.noreply.github.com>
2026-02-05 17:41:10 +0100

    Replace push-notification request channelId with channelName (#287)

--- src/api/versions/v1/enums/notification-channel-enum.ts
+++ src/api/versions/v1/enums/notification-channel-enum.ts
@@ -3,3 +3,18 @@ export enum NotificationChannelType {
   Menu = 1,
   Match = 2,
 }
+
+export enum NotificationChannelName {
+  Global = "GLOBAL",
+  Menu = "MENU",
+  Match = "MATCH",
+}
+
+export const NotificationChannelNameToType: Record<
+  NotificationChannelName,
+  NotificationChannelType
+> = {
+  [NotificationChannelName.Global]: NotificationChannelType.Global,
+  [NotificationChannelName.Menu]: NotificationChannelType.Menu,
+  [NotificationChannelName.Match]: NotificationChannelType.Match,
+};
