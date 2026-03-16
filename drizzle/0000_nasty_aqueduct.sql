850253604f37329f3e982e1a1368355b15300561
Miguel Ripoll <3296866+MiguelRipoll23@users.noreply.github.com>
2026-03-16 16:33:41 +0100

    Require role to be created manually (#337)

--- drizzle/0000_nasty_aqueduct.sql
+++ drizzle/0000_nasty_aqueduct.sql
@@ -1,4 +1,3 @@
-CREATE ROLE "authenticated_user";--> statement-breakpoint
 CREATE TABLE "blocked_words" (
 	"id" integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY (sequence name "blocked_words_id_seq" INCREMENT BY 1 MINVALUE 1 MAXVALUE 2147483647 START WITH 1 CACHE 1),
 	"word" varchar(255) NOT NULL,
